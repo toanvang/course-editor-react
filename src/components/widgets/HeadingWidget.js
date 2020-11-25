83470193f6199e62bad1053ad4caa9b3a4515626
Toan <tvang994@gmail.com>
2020-11-25 12:59:18 -0800

    fix for a5

--- src/components/widgets/HeadingWidget.js
+++ src/components/widgets/HeadingWidget.js
@@ -84,7 +84,31 @@ const HeadingWidget = ({widget, topicId,  widgets, upWidget, downWidget, nameWid
                 </div>
                 <button onClick={() => updateWidget(widget)}
                     type="submit" className="btn btn-sm btn-success">Save</button>
-
+                <h4>Preview</h4>
+                {
+                    widget.size === "1" &&
+                    <h1>{widget.text}</h1>
+                }
+                {
+                    widget.size === "2" &&
+                    <h2>{widget.text}</h2>
+                }
+                {
+                    widget.size === "3" &&
+                    <h3>{widget.text}</h3>
+                }
+                {
+                    widget.size === "4" &&
+                    <h4>{widget.text}</h4>
+                }
+                {
+                    widget.size === "5" &&
+                    <h5>{widget.text}</h5>
+                }
+                {
+                    widget.size === "6" &&
+                    <h6>{widget.text}</h6>
+                }
             </form>
 
         </div>
--- src/components/widgets/ParagraphWidget.js
+++ src/components/widgets/ParagraphWidget.js
@@ -71,6 +71,32 @@ const ParagraphWidget = ({widget, widgets,downWidget, upWidget, nameWidget, text
                     </button>
 
                 </form>
+                    <h4>Preview</h4>
+                    {
+                            widget.size === "1" &&
+                            <h1>{widget.text}</h1>
+
+                    }
+                    {
+                            widget.size === "2" &&
+                            <h2>{widget.text}</h2>
+                    }
+                    {
+                            widget.size === "3" &&
+                            <h3>{widget.text}</h3>
+                    }
+                    {
+                            widget.size === "4" &&
+                            <h4>{widget.text}</h4>
+                    }
+                    {
+                            widget.size === "5" &&
+                            <h5>{widget.text}</h5>
+                    }
+                    {
+                            widget.size === "6" &&
+                            <h6>{widget.text}</h6>
+                    }
             </div>
         }
         {
